0d0cfac3ee5a2fdf40d18f41de43d4be782f6db1
Mat& <mbolszewski@student.agh.edu.pl>
2023-01-10 11:53:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const util = require("util");
 const mysql = require("mysql");
+var path = require("path");
 
 const app = express();
 const port = process.env.PORT || 5001;
@@ -16,18 +17,16 @@ const query = util.promisify(conn.query).bind(conn);
 
 app.use(express.json());
 
-require("./routes/bitrate")(app, query);
-require("./routes/event")(app, query);
-require("./routes/experiment")(app, query);
-require("./routes/playback_data")(app, query);
-require("./routes/resoult")(app, query);
-require("./routes/video")(app, query);
+require(path.join(__dirname + "/routes/bitrate"))(app, query);
+require(path.join(__dirname + "/routes/event"))(app, query);
+require(path.join(__dirname + "/routes/experiment"))(app, query);
+require(path.join(__dirname + "/routes/playback_data"))(app, query);
+require(path.join(__dirname + "/routes/resoult"))(app, query);
+require(path.join(__dirname + "/routes/video"))(app, query);
 
 app.get("/", (req, res) => {
   res.send(`App listening on port ${port}`);
 });
-
-// app.use(express.static(path.join(__dirname, "routes")));
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
